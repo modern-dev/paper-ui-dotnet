718639e73ee9893f8107f48ddbb0240926128ba8
Bohdan Shtepan <winsatana@ya.ru>
2016-03-23 02:05:05 +0200

    Add IPaperShadowElement interface.

---
+++ ModernDev.PaperUI.Shared/Interfaces/IPaperShadowElement.cs
@@ -0,0 +1,8 @@
+namespace ModernDev.PaperUI
+{
+    public interface IPaperShadowElement
+    {
+        ShadowElevations Elevation { get; set; }
+        double CornerRadius { get; set; }
+    }
+}
