3218235c0450a1ed4f3db3b5e2bcf897b48c7881
Bohdan Shtepan <winsatana@ya.ru>
2016-03-23 02:04:10 +0200

    Add ShadowElevationData class.

---
+++ ModernDev.PaperUI.Shared/Model/ShadowElevationData.cs
@@ -0,0 +1,16 @@
+﻿namespace ModernDev.PaperUI
+{
+    public sealed class ShadowElevationData
+    {
+        public ShadowElevationData(double opacity, int depth, int radius)
+        {
+            Opacity = opacity;
+            ShadowDepth = depth;
+            BlurRadius = radius;
+        }
+
+        public double Opacity { get; }
+        public int ShadowDepth { get; }
+        public int BlurRadius { get; }
+    }
+}
