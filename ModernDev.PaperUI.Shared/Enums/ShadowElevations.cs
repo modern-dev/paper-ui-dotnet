f8f6a116bf2995013f4d1e8cbf20bbed8545fe6f
Bohdan Shtepan <winsatana@ya.ru>
2016-03-23 02:04:25 +0200

    Add ShadowElevations enum.

---
+++ ModernDev.PaperUI.Shared/Enums/ShadowElevations.cs
@@ -0,0 +1,12 @@
+﻿namespace ModernDev.PaperUI
+{
+    public enum ShadowElevations
+    {
+        None = 0,
+        Depth1 = 1,
+        Depth2 = 2,
+        Depth3 = 3,
+        Depth4 = 4,
+        Depth5 = 5
+    }
+}
